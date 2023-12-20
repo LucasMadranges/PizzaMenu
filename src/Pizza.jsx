69040c3cb28f0f371432136f6b329acffb4da989
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-12-20 17:43:19 +0100

    Upgrade Pizza.jsx

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,31 +1,15 @@
 export default function Pizza({pizza}) {
-    if (pizza.soldOut) {
-        return (
-            <>
-                <li className="pizza sold-out">
-                    <img src={pizza.photoName}
-                         alt={pizza.name}/>
-                    <div>
-                        <h3>{pizza.name}</h3>
-                        <p>{pizza.ingredients}</p>
-                        <span>{pizza.price}€</span>
-                    </div>
-                </li>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <li className="pizza">
-                    <img src={pizza.photoName}
-                         alt={pizza.name}/>
-                    <div>
-                        <h3>{pizza.name}</h3>
-                        <p>{pizza.ingredients}</p>
-                        <span>{pizza.price}€</span>
-                    </div>
-                </li>
-            </>
-        )
-    }
+    return (
+        <>
+            <li className={`pizza ${pizza.soldOut ? "sold-out" : ''}`}>
+                <img src={pizza.photoName}
+                     alt={pizza.name}/>
+                <div>
+                    <h3>{pizza.name}</h3>
+                    <p>{pizza.ingredients}</p>
+                    <span>{pizza.soldOut ? "Sold Out!" : `${pizza.price}€`}</span>
+                </div>
+            </li>
+        </>
+    )
 }
